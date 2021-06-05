a723f076973319336598fd18da67db380e774a09
Fernando <fernando.herrera85@gmail.com>
2021-06-05 10:11:56 -0400

    Update 07-clase-basica.dart

--- 07-clase-basica.dart
+++ 07-clase-basica.dart
@@ -1,31 +1,31 @@
-void main() {
+void main(){
   
-  final wolverine = new Heroe( 
-    poder: 'Regeneración',
-    nombre: 'Logan'
-  );
-    
+  final wolverine = new Heroe(nombre:'Logan', poder: 'Regeneración');
+  
+//   wolverine.nombre = 'Logan';
+//   wolverine.poder = 'Regeneración';
+  
   print( wolverine );
   
 }
+
 
 class Heroe {
   
   String nombre;
   String poder;
   
-//   Heroe({ String nombre = 'Sin Nombre', String poder }) {
-//     this.nombre = nombre;
-//     this.poder  = poder; 
-//   }
-  
   Heroe({ 
     required this.nombre, 
     required this.poder 
   });
   
-  String toString() => 'nombre: $nombre - poder: $poder';
+//   Heroe( String pNombre ) {
+//     this.nombre = pNombre;
+//   }
   
+  
+  String toString() {
+    return 'Heroe: nombre: ${this.nombre}, poder: ${ this.poder }';
+  }
 }
-
-
