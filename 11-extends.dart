f0a05bf75090473b2795f46a0653ec9d7a3be319
Fernando <fernando.herrera85@gmail.com>
2021-06-05 11:10:58 -0400

    Update 11-extends.dart

--- 11-extends.dart
+++ 11-extends.dart
@@ -1,44 +1,44 @@
 void main() {
-  
-  // ERROR: no puede ser inicializada
-  // final fernando = new Personaje();
-
+    
   final superman = new Heroe('Clark Kent');
-  final luthor = new Villano('Lex Luthor');
+  final luthor   = new Villano('Lex Luthor');
+ 
   
-
   print( superman );
   print( luthor );
-  
-  
 }
-
 
 
 abstract class Personaje {
   String? poder;
   String nombre;
-
+  
   Personaje( this.nombre );
-
+  
   @override
   String toString() {
     return '$nombre - $poder';
   }
+  
 }
 
 
-class Heroe extends Personaje { 
-	
-  int? valentia;
-
-  Heroe( String nombre ) : super(nombre);
-
+class Heroe extends Personaje {
+ 
+   int valentia = 100;
+  
+   Heroe( String nombre ): super( nombre );
+  
 }
 
 class Villano extends Personaje {
-
-  int? maldad;
-
-  Villano( String nombre ) : super(nombre);
+  
+  int maldad = 50;
+  
+  Villano( String nombre ): super( nombre );
 }
+
+
+
+
+
